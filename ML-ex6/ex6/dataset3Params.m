82a745c3c320441d1614945de8efebc26729a945
Isaac Gutierrez <gutierrez.isaac@gmail.com>
2020-05-13 20:05:42 -0500

    Implemented Gaussian Kernel and dataset3Params

--- ML-ex6/ex6/dataset3Params.m
+++ ML-ex6/ex6/dataset3Params.m
@@ -11,6 +11,9 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+p1 = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+p2 = [0.03; 0.1; 0.3; 1; 3; 9; 27; 81] ;
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -23,11 +26,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+[p,q] = ndgrid(p1,p2);
+params = [p(:),q(:)];
 
+m = size(params,1);
+min_error = Inf;
+best_params = [0,0];
+for l=1:m
+    model= svmTrain(X, y, params(l,1), @(x1, x2) gaussianKernel(x1, x2, params(l,2)));
+    predictions= svmPredict(model, Xval);
+    model_error = mean(double(predictions ~= yval));
+    if(model_error < min_error)
+        min_error = model_error;
+        best_params = params(l,:);
+    end
+end
 
-
-
-
+C = best_params(1);
+sigma = best_params(2);      
 
 % =========================================================================
 
